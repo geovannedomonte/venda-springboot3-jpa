39cd6223f7d7372f85698bf71abf83e3542084db
Geovanne do Monte <geovannedomonte@gmail.com>
2024-10-14 22:21:11 -0300

    Adicionando a camada de Service e implementando Injeção de dependencia com Registro de componente

--- src/main/java/com/springbootjpa/venda/config/TestConfig.java
+++ src/main/java/com/springbootjpa/venda/config/TestConfig.java
@@ -1,6 +1,5 @@
 package com.springbootjpa.venda.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/springbootjpa/venda/resources/UsuarioResource.java
+++ src/main/java/com/springbootjpa/venda/resources/UsuarioResource.java
@@ -1,19 +1,33 @@
 package com.springbootjpa.venda.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootjpa.venda.entities.Usuario;
+import com.springbootjpa.venda.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
-
+	
+	@Autowired
+	public UsuarioService service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
-		Usuario u = new Usuario(1L, "Geovanne", "geo@gmail.com", "219878787", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
